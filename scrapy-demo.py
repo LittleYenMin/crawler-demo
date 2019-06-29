ea10de48923dc1f9f7c17356ddf22d4db2bd41c6
LittleYenMin <fkym08@gmail.com>
2019-06-29 14:53:03 +0800

    Scrapy demo, element select by xpath.

---
+++ scrapy-demo.py
@@ -0,0 +1,19 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = 'quotes'
+    start_urls = [
+        'http://quotes.toscrape.com/tag/humor/',
+    ]
+
+    def parse(self, response):
+        for quote in response.xpath('//div[contains(@class, "quote")]'):
+            yield {
+                'text': quote.xpath('span[contains(@class, text)]/text()').get(),
+                'author': quote.xpath('span/small/text()').get(),
+            }
+
+        next_page = response.css('//li[contains(@class, "next")]/a/@href').get()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse)
